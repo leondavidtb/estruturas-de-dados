71cc6503e2c011c9b8920c98e361b00377cf5cd1
Leonardo David <43556507+leondavidtb@users.noreply.github.com>
2018-12-17 22:18:20 -0300

    Create Main.java

---
+++ AVL/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avl;
+
+/**
+ *
+ * @author leon
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Menu m = new Menu();
+        m.gerarMenu();
+
+    }
+}
